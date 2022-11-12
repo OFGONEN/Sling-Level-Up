12635ddee37b6e292ee9c936633426e4c8c7996a
ofgonen-home <ofgonen@gmail.com>
2022-11-12 18:44:50 +0300

    Implement Enemy Script

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,35 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class Enemy : MonoBehaviour
+{
+#region Fields
+  [ Title( "Shared Variables" ) ]
+    [ SerializeField ] SharedIntNotifier notif_stickman_power;
+
+  [ Title( "Setup" ) ]
+    [ SerializeField ] float enemy_power;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
