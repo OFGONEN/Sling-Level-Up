3adaa4e3eb7d55806428b1c8b75ba42398c7fdc1
ofgonen_office <ofgonen@gmail.com>
2022-11-14 13:36:48 +0300

    LaunchDirection: Target

--- Assets/Script/LaunchDirection.cs
+++ Assets/Script/LaunchDirection.cs
@@ -9,18 +9,73 @@ using Sirenix.OdinInspector;
 public class LaunchDirection : MonoBehaviour
 {
 #region Fields
+  [ Title( "Shared Variables" ) ]
+    [ SerializeField ] SharedReferenceNotifier notif_stickman_reference;
+    [ SerializeField ] SharedVector2 shared_finger_delta_direction;
+	[ SerializeField ] SharedFloat shared_finger_delta_magnitude;
+
+  [ Title( "Components" ) ]
+    [ SerializeField ] Transform gfx;
+    [ SerializeField ] GameObject[] gfx_child_array;
+    [ SerializeField ] Transform target;
+
+
+    UnityMessage onUpdate;
 #endregion
 
 #region Properties
 #endregion
 
 #region Unity API
+    void OnDisable()
+    {
+		onUpdate = ExtensionMethods.EmptyMethod;
+	}
+
+    void Awake()
+    {
+		onUpdate = ExtensionMethods.EmptyMethod;
+	}
+
+    void Update()
+    {
+		onUpdate();
+	}
 #endregion
 
 #region API
+    public void OnStickmanLaunchStart()
+    {
+		target.localPosition = Vector3.right;
+		DisableLaunchVisual();
+	}
+
+	public void OnStickmanLaunchEnd()
+	{
+		onUpdate = ExtensionMethods.EmptyMethod;
+		DisableLaunchVisual();
+	}
 #endregion
 
 #region Implementation
+    void OnLaunchUpdate()
+    {
+		target.localPosition = shared_finger_delta_direction.sharedValue;
+		OnLaunchUpdate_Visual();
+	}
+
+    void OnLaunchUpdate_Visual()
+    {
+
+    }
+
+    void DisableLaunchVisual()
+    {
+        for( var i = 0; i < gfx_child_array.Length; i++ )
+        {
+			gfx_child_array[ i ].SetActive( false );
+		}
+    }
 #endregion
 
 #region Editor Only
@@ -30,23 +85,24 @@ public class LaunchDirection : MonoBehaviour
     {
 		UnityEditor.EditorUtility.SetDirty( this );
 
-		var gfx = transform.GetChild( 0 );
 		gfx.DestroyAllChildren();
+		gfx_child_array = new GameObject[ count ];
 
-        for( var i = 0; i < count; i++ )
+		for( var i = 0; i < count; i++ )
         {
 			var instance = UnityEditor.PrefabUtility.InstantiatePrefab( prefab ) as GameObject;
 			instance.name = instance.name + "_" + ( i + 1 );
 			instance.transform.SetParent( gfx );
 			instance.transform.localRotation = Quaternion.identity;
 			instance.transform.localPosition = Vector3.right * i * space;
+
+			gfx_child_array[ i ] = instance;
 		}
 	}
 
     [ Button() ]
     void ChangeSizeLaunchDirectionVisual( float start, float end )
     {
-		var gfx        = transform.GetChild( 0 );
 		var childCount = gfx.childCount;
 
 		for( var i = 0; i < gfx.childCount; i++ )
